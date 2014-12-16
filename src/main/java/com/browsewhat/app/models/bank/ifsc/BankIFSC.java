f56ba7d028149339054e7f058a15ea120dbed386
Rahul <RKrishna@asicentral.com>
2014-12-16 13:57:05 +0530

    IFSCModel for POJO

---
+++ src/main/java/com/browsewhat/app/models/bank/ifsc/BankIFSC.java
@@ -0,0 +1,12 @@
+package com.browsewhat.app.models.bank.ifsc;
+
+import java.io.Serializable;
+
+public class BankIFSC implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3848331499271863949L;
+
+}
