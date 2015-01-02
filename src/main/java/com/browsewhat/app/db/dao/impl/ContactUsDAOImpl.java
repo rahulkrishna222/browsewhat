ac208982654e5c807c48cd359487455085f9a5e3
Rahul <RKrishna@asicentral.com>
2015-01-02 20:00:48 +0530

    Moved DAO classes to correct path.

---
+++ src/main/java/com/browsewhat/app/db/dao/impl/ContactUsDAOImpl.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package com.browsewhat.app.db.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.browsewhat.app.db.dao.ContactUsDAO;
+import com.browsewhat.app.db.entities.BwContactus;
+
+/**
+ * @author krahul
+ *
+ */
+public class ContactUsDAOImpl implements ContactUsDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.browsewhat.app.db.dao.template.DAOTemplate#findAll()
+     */
+    @Override
+    public List<BwContactus> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.browsewhat.app.db.dao.template.DAOTemplate#insert(java.lang.Object)
+     */
+    @Override
+    public BwContactus insert(BwContactus t) {
+        Session session = sessionFactory.openSession();
+
+        session.save(t);
+
+        return t;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.browsewhat.app.db.dao.template.DAOTemplate#delete(java.lang.Object)
+     */
+    @Override
+    public void delete(BwContactus id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.browsewhat.app.db.dao.template.DAOTemplate#find(java.lang.Object)
+     */
+    @Override
+    public BwContactus find(Object id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.browsewhat.app.db.dao.template.DAOTemplate#update(java.lang.Object)
+     */
+    @Override
+    public BwContactus update(BwContactus t) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
